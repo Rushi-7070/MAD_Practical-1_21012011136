0877da2b1ae534bb7e9b54a56e6e23ae5cfdffb8
Rushi-7070 <97504709+Rushi-7070@users.noreply.github.com>
2023-07-20 17:11:29 +0530

    Add files via upload
    q 1.7 added

---
+++ src/main/kotlin/Practical-1.7.kt
@@ -0,0 +1,16 @@
+fun main(){
+    print("Enter the number:")
+    var num = readLine()!!.toInt();
+    var factorial = fact(num)
+    print("factorial of $num = $factorial")
+
+}
+
+fun fact(num:Int):Long{
+    if(num==0 || num ==1){
+        return 1;
+    }
+    else
+        return num*fact(num-1)
+
+}
