8ffa685ec042568c490fa524cbeff248d1ff8cef
student <student@gnu.ac.in>
2023-07-21 08:44:35 +0530

    Practical-1.8.kt

---
+++ src/main/kotlin/Practical-1.8.kt
@@ -0,0 +1,37 @@
+import java.util.Arrays
+
+fun main(){
+    println("Create array using arrayof method:")
+    var a1= arrayOf(10,25,45,62,35,36)
+    println(a1.contentDeepToString())
+    print("Print Using for loop:")
+    for (i in 0..a1.size-1){
+        print(" "+a1[i])
+    }
+    println("\nCreate array using array<>() method:")
+    var a2=Array<Int>(6){0}
+    println(Arrays.deepToString(a2))
+
+    println("\nCreate array using Array<>() and lamda function:")
+    var a3=Array<Int>(8){i:Int->i}
+    println(Arrays.deepToString(a3))
+
+    println("\nCreate array using IntArray():")
+    var a4=IntArray(5){0}
+    println(a4.contentToString())
+
+    println("\nCreate array using intArrayof():")
+    var a5= intArrayOf(12,51,23,56,84,24)
+    println(a5.joinToString(" "))
+
+    println("\nCreate 2D array using arrayof() and intArrayof():")
+    var a6= arrayOf(intArrayOf(1,5), intArrayOf(5,9), intArrayOf(6,4), intArrayOf(70,15))
+    println(a6.contentDeepToString())
+
+    println("\nCreate array using user input:")
+    println("Enter the values of Array:")
+    var a7= Array<Int>(5){ readLine()!!.toInt() }
+    print("Entered Array is:")
+    println(a7.contentDeepToString())
+
+}
