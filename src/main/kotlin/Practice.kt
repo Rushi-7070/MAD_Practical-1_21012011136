bac9f049803669aec861cfda494cae1f919cc9d6
Rushi-7070 <97504709+Rushi-7070@users.noreply.github.com>
2023-07-26 11:09:51 +0530

    Add files via upload

---
+++ src/main/kotlin/Practice.kt
@@ -0,0 +1,9 @@
+class Student (var er_no : String, var name : String) {
+
+}
+
+fun main () {
+    val a = Student ("21012011136", "Rushi Savani")
+    println("Enrollment No.: "+a.er_no)
+    println("Name : "+a.name)
+}
